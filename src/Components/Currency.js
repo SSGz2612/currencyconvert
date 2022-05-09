a677731f6e94ff2a6de3efd64a8b5723aae6a748
Sergio <sduarte2612@gmail.com>
2022-05-09 12:06:26 +0300

    fix some docs

---
+++ src/Components/Currency.js
@@ -0,0 +1,28 @@
+import '../Currency.css';
+
+function Currency( props ) {
+    return (
+        <div className="currency">
+            <input
+                className="input"
+                type="text"
+                value={ props.val }
+                onChange={ e => props.onValChange( e.target.value )}
+            />
+
+            <div className='country'><div className='countryfl'>{ props.flag }</div></div>
+
+            <select
+                className="select"
+                value={ props.coins }
+                onChange={ e => props.onCoinsChange( e.target.value )}
+            >{
+                props.totalCoins.map(( e, index ) => (
+                    <option key={ index } value={ e }>{ e }</option>
+                ))
+            }</select>
+        </div>
+    )
+}
+
+export default Currency;
